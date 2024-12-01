27e387208ef0f450baac691de0d66f2b3475c04f
nurlanua <73550146+nurlanua@users.noreply.github.com>
2024-11-30 22:42:18 -0600

    Update swagger-initializer.js

--- sprints/sprint-3/dist/swagger-initializer.js
+++ sprints/sprint-3/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://raw.githubusercontent.com/COMP-4350-Group-8/admin/refs/heads/main/sprints/sprint-3/dist/documentation.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
